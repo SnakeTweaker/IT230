48dfdb0c5fbf1b1e320e5314ea6cdf6b6125976e
Metal Fist Snake "Busca <77250853+SnakeTweaker@users.noreply.github.com>
2021-08-23 05:54:38 +0000

    Add files via upload

---
+++ Mod 2 Coding Busca/2-2 coding/2-2 coding/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *Assignment: Coding Activity Module 2
+ *Class: IT-230
+ *Author: CJ Busca
+ *Date: 21136
+ * Instructor: Dr. Wetsch
+ */
+
+
+using System;
+using System.Linq;
+
+
+namespace _2_2_coding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring the array and the integers within
+            int[] arrNums = new int[] { 13, 55, 123, 325 };
+            int sum = arrNums.Sum();
+            int product = arrNums.Aggregate((runningProduct, nextFactor) => runningProduct * nextFactor);
+
+
+            //Print functions
+            Console.WriteLine("The sum of 13, 55, 123, and 325 = " + sum);
+            Console.WriteLine("The product of 13, 55, 123, and 325 = " + product);
+
+        }
+    }
+}
