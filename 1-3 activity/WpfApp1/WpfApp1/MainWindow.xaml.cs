ef6b2892017695162d71d2fc0c5d76af42190307
Metal Fist Snake "Busca <77250853+SnakeTweaker@users.noreply.github.com>
2021-08-23 05:55:13 +0000

    Add files via upload

---
+++ 1-3 activity/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+CJ Busca
+IT-230 Software Development with C#.NET
+05/04/2021
+1-3 Coding Activity: Your First C# WPF Application (HelloYourName)
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloYourName
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void okClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello " + userName.Text + " and welcome to C#!");
+        }
+    }
+}
